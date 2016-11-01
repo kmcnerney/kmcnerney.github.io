92a5109f703536fa80433cf970de1023f2edaa1e
McNerney, Kevin <kmcnerney@crowdtorch.com>
2016-11-01 00:45:32 -0400

    initial styling and countdown

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { Countdown } from './components/counter';
 
 export class App extends React.Component {
   render() {
+    let centeredStyle = {
+      textAlign: "center"
+    };
+
     return (
-      <div>
-        <Countdown className="centered" />
+      <div style={centeredStyle}>
+        <img width="250" height="350" src={"https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/388050_269056213136987_62927847_n.jpg?oh=bfae77dfaf54d408438173e8efdf81fa&oe=589F2F57"} />
+        <Countdown />
       </div>
     );
   }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const ONE_SECOND = 1000;
+const NIC_RETURN_DATE = '2016/11/03 08:45 PM';
 
 export class Countdown extends React.Component {
   constructor(props) {
     super(props);
 
-    let diff = Math.abs(new Date() - new Date('2016/11/03 08:45 PM'));
+    let diff = Math.abs(new Date() - new Date(NIC_RETURN_DATE));
     this.state = { countdown: diff };
 
     setInterval(() => this.tick(), ONE_SECOND);
@@ -30,9 +31,9 @@ export class Countdown extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="text-center">
         <h3>Nic returns to glory in...</h3>
-        <h1>{this.state.days} days, {this.state.hours} hours, {this.state.minutes} minutes, and {this.state.seconds} seconds</h1>
+        <h1 style={{color:"#FF1493"}}>{this.state.days} days, {this.state.hours} hours, {this.state.minutes} minutes, and {this.state.seconds} seconds</h1>
       </div>
     );
   }
