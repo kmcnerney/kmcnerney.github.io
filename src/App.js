49299b83e95153f73e9c187306e563b73821b973
McNerney, Kevin <kmcnerney@crowdtorch.com>
2016-11-01 00:56:17 -0400

    show other option

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ export class App extends React.Component {
       <div style={centeredStyle}>
         <img width="250" height="350" src={"https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/388050_269056213136987_62927847_n.jpg?oh=bfae77dfaf54d408438173e8efdf81fa&oe=589F2F57"} />
         <Countdown />
+
+        ^ That's my 5 minute version. Maybe I'll use some existing stuff and make a cooler site tomorrow
+        <iframe width="484" height="249" src="https://w2.countingdownto.com/1577992" frameborder="0"></iframe>
       </div>
     );
   }
