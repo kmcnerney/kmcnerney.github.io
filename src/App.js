88ceabad1b8f96763b716e5435cebfea8add129e
Kevin McNerney <kevin.m.mcnerney@gmail.com>
2023-09-28 13:12:02 -0400

    fix redirect uri

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import './App.css';
-import axios from 'axios';
+import './App.css'
+import axios from 'axios'
+import React from 'react'
+
+const HOMEPAGE = 'https://guillotine.football'
+const YAHOO_CLIENT_ID = 'dj0yJmk9NGVBekNWbUplUndJJmQ9WVdrOWNYTldWakZUWlhRbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTJk'
+const YAHOO_CLIENT_SECRET = '67ff91fb2b422cd6fe3a947fe4dadb4ace874b2b'
+const CORS_PROXY_URL = 'https://corsproxy.io'
+const YAHOO_API_HOST = 'https://api.login.yahoo.com'
 
 async function yahooLogin(code) {
   console.log(`authing with yahoo using code: ${code}`)
 
   try {
-    const proxyurl = 'https://corsproxy.io/?'
-    const token = await axios.post(proxyurl + 'https://api.login.yahoo.com/oauth2/get_token', 
+    const token = await axios.post(`${CORS_PROXY_URL}?${YAHOO_API_HOST}/oauth2/get_token`, 
       {
         grant_type: 'authorization_code',
         code,
-        redirect_uri: 'https://kmcnerney.github.io/fantasy-football',
-        client_id: 'dj0yJmk9NGVBekNWbUplUndJJmQ9WVdrOWNYTldWakZUWlhRbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTJk',
-        client_secret: '67ff91fb2b422cd6fe3a947fe4dadb4ace874b2b'
+        redirect_uri: HOMEPAGE,
+        client_id: YAHOO_CLIENT_ID,
+        client_secret: YAHOO_CLIENT_SECRET
       },
-      {headers: {'content-type': 'application/x-www-form-urlencoded'}}
-    )
+      {headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      }
+    })
 
     console.log(`yahoo auth succeeded and got token`, token)
     return token
@@ -25,30 +33,49 @@ async function yahooLogin(code) {
 
 }
 
-function App() {
-  const search = window.location.search;
-  const params = new URLSearchParams(search);
-  const yahoo_code = params.get('code');
-
-  if (yahoo_code) {
-    yahooLogin(yahoo_code);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yahooApiToken: window.sessionStorage.getItem('yahoo_api_token')
+    };
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'https://i.insider.com/651146b1e2c0220019ef3ba5?width=500'} alt='TayTay' />
-        <p>
-          Welcome to the Guillotine League
-        </p>
-        <h1>
-          <a href={'https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9NGVBekNWbUplUndJJmQ9WVdrOWNYTldWakZUWlhRbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTJk&redirect_uri=https://kmcnerney.github.io/fantasy-football&response_type=code'}>
-            Login here
-          </a>
-        </h1>
-      </header>
-    </div>
-  );
-}
+  async componentDidMount() {
+    console.log('in App.componentDidMount', this.state)
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    const yahooAuthRequestCode = params.get('code')
+    if (this.state.yahooApiToken) {
+      console.log('already logged into yahoo')
+      return
+    }
+    if (yahooAuthRequestCode) {
+      const authResponse = await yahooLogin(yahooAuthRequestCode)
+      const token = authResponse?.data?.access_token 
+      window.sessionStorage.setItem('yahoo_api_token', token)
+      this.setState({
+        yahooApiToken: token
+      })
+    }
+  }
 
-export default App;
+  render() {
+    console.log('in App.render', this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={'https://i.insider.com/651146b1e2c0220019ef3ba5?width=500'} alt='TayTay' />
+          <p>
+            Welcome to the Guillotine League
+          </p>
+          <h1>
+            <a href={`${YAHOO_API_HOST}/oauth2/request_auth?client_id=${YAHOO_CLIENT_ID}&redirect_uri=${HOMEPAGE}&response_type=code`}>
+              Login here
+            </a>
+          </h1>
+        </header>
+      </div>
+    )
+  }
+}
